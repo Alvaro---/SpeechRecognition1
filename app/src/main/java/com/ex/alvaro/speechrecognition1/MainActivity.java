2e9f9d19ac9d33c3ae15a67b6a5cd50a228c383e
Alvaro Mercado <alvaroawake@gmail.com>
2015-02-23 14:29:25 -0400

    l-23

--- app/src/main/java/com/ex/alvaro/speechrecognition1/MainActivity.java
+++ app/src/main/java/com/ex/alvaro/speechrecognition1/MainActivity.java
@@ -1,39 +1,61 @@
 package com.ex.alvaro.speechrecognition1;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.speech.tts.TextToSpeech;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends Activity implements OnClickListener, TextToSpeech.OnInitListener {
+
+    private static final int VR_REQUEST=999;
+    private ListView wordList;
+    private final String LONG_TAG="SpeechReapeatActivity";
+    //TTS
+    private int MY_DATA_CHECK_CODE=0;
+    private TextToSpeech repeatTTS;
+
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boton=(Button)findViewById(R.id.btnHablar);
+        wordList=(ListView)findViewById(R.id.word_list);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onClick(View v) {
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onInit(int status) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
